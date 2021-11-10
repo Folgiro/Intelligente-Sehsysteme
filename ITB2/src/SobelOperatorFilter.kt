77bf0e7ab636874304b3b07a07dd04dd430cc40a
Christian Schultze <christian.schultze98@gmx.de>
2021-11-10 11:50:54 +0100

    fix gradient calculation

--- ITB2/src/SobelOperatorFilter.kt
+++ ITB2/src/SobelOperatorFilter.kt
@@ -1,20 +1,23 @@
 import itb2.filter.AbstractFilter
 import itb2.image.Image
 import itb2.image.ImageFactory
+import kotlin.math.atan
+import kotlin.math.pow
+import kotlin.math.sqrt
 
-class SobelOperatorFilter : AbstractFilter() {
+open class SobelOperatorFilter : AbstractFilter() {
     /**
      * returns values of the absolute gradient which have been distributed over the whole intensity spectrum
      */
     protected fun calculateAbsoluteGradient(horizontal: Array<DoubleArray>, vertical: Array<DoubleArray>, width: Int, height: Int): Array<DoubleArray> {
         val result = Array(width) { DoubleArray(height) }
-        var min = Math.sqrt(Math.pow(horizontal[0][0], 2.0) + Math.pow(vertical[0][0], 2.0))
+        var min = Math.sqrt(horizontal[0][0].pow(2.0) + vertical[0][0].pow(2.0))
         var max = min
 
         //calulate absolute gradient and min max
         for (col in 0 until width) {
             for (row in 0 until height) {
-                result[col][row] = Math.sqrt(Math.pow(horizontal[col][row], 2.0) + Math.pow(vertical[col][row], 2.0))
+                result[col][row] = sqrt(horizontal[col][row].pow(2.0) + vertical[col][row].pow(2.0))
                 min = Math.min(min, result[col][row])
                 max = Math.max(max, result[col][row])
             }
@@ -30,7 +33,9 @@ class SobelOperatorFilter : AbstractFilter() {
         for (col in 0 until width) {
             for (row in 0 until height) {
                 // save angle degrees into array, value of 10000 is a placeholder for no Gradient
-                result[col][row] = if (horizontal[col][row] == 0.0) if (vertical[col][row] == 0.0) 0.0 else 90.0 else Math.toDegrees(Math.atan(Math.abs(vertical[col][row] / horizontal[col][row])))
+                val tmp = if (horizontal[col][row] == 0.0) if (vertical[col][row] == 0.0) 0.0 else 90.0 else Math.toDegrees(
+                    atan(vertical[col][row] / horizontal[col][row]))
+                result[col][row] = if(tmp < 0) tmp + 180 else tmp
             }
         }
         return result
@@ -51,13 +56,13 @@ class SobelOperatorFilter : AbstractFilter() {
                 Utility.doubleArrayToImage(result, output, width, height)
             }
             "Orientierung" -> {
-                output = ImageFactory.getPrecision(input).rgb(input.getSize())
+                output = ImageFactory.getPrecision(input).rgb(input.size)
                 result = calculateGradientAngle(horizontal, vertical, width, height)
                 Utility.angleBinning(result, output, width, height)
             }
             "Gewichtete Orientierung" -> {
                 val absoluteGradient = calculateAbsoluteGradient(horizontal, vertical, width, height)
-                output = ImageFactory.getPrecision(input).rgb(input.getSize())
+                output = ImageFactory.getPrecision(input).rgb(input.size)
                 result = calculateGradientAngle(horizontal, vertical, width, height)
                 Utility.angleBinning(result, output, width, height, absoluteGradient)
             }
