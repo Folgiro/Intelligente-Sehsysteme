bd51cc70c59d67cbac882b099e1144d55fe8aa6a
Christian Schultze <christian.schultze98@gmx.de>
2021-11-27 11:58:13 +0100

    add task 1A

--- ITB2/src/ScissorsFilter.java
+++ ITB2/src/ScissorsFilter.java
@@ -1,11 +1,14 @@
+import itb2.engine.Controller;
 import itb2.filter.AbstractFilter;
+import itb2.image.DrawableImage;
 import itb2.image.Image;
 import itb2.image.ImageFactory;
+import java.awt.Point;
 
-import java.util.ArrayList;
-
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ScissorsFilter extends AbstractFilter {
 
@@ -25,38 +28,34 @@ public class ScissorsFilter extends AbstractFilter {
                 ArrayList<Integer> neighbors = new ArrayList<>(4);
                 for (int offset = -1; offset < 2; offset += 2) {
                     if (!(col + offset < 0 || col + offset >= width)) {
-                        neighbors.add(width * (col + offset) + row);
+                        neighbors.add(height * (col + offset) + row);
                     }
-                    if (!(row + offset < 0 || row + offset >= width)) {
-                        neighbors.add(width * col + (row + offset));
+                    if (!(row + offset < 0 || row + offset >= height)) {
+                        neighbors.add(height * col + (row + offset));
                     }
                 }
-                result.add(new Vertex(listToArray(neighbors), absValues[col][row] - (absValues[start[0]][start[1]] + absValues[end[0]][end[1]])));
+                result.add(new Vertex(listToArray(neighbors), Math.abs(absValues[col][row] - (absValues[start[0]][start[1]] + absValues[end[0]][end[1]]))));
             }
         }
         return result;
     }
 
 
-    protected ArrayList<Integer> executeDijkstra(ArrayList<Vertex> graph, int start, int end) {
-        double max_cost = 0;
-        for(Vertex v : graph){
-            max_cost += v.getCost() + 1;
-        }
+    protected ArrayList<Integer> executeDijkstra(ArrayList<Vertex> graph, final int start, final int end, int heigth) {
         double[] pathCosts = new double[graph.size()];
-        Arrays.fill(pathCosts, max_cost);
+        Arrays.fill(pathCosts, Double.POSITIVE_INFINITY);
         pathCosts[start] = graph.get(start).getCost();
 
         ArrayList<Integer> activeNodes = new ArrayList<>(1);
         activeNodes.add(start);
 
         int min = -1;
-        while(min != end){
+        while (min != end) {
             min = selectDeleteMin(activeNodes);
             Vertex vMin = graph.get(min);
 
-            for(int v : vMin.getAdjacencyArray()){
-                if(pathCosts[min] + graph.get(v).getCost() < pathCosts[v]){
+            for (int v : vMin.getAdjacencyArray()) {
+                if (pathCosts[min] + graph.get(v).getCost() < pathCosts[v]) {
                     pathCosts[v] = pathCosts[min] + graph.get(v).getCost();
                     activeNodes.add(v);
                 }
@@ -65,29 +64,52 @@ public class ScissorsFilter extends AbstractFilter {
         }
 
         //back tracing
+        ArrayList<Integer> visitedNodes = new ArrayList<>(1);
         ArrayList<Integer> nodeList = new ArrayList<>(1);
-        activeNodes.add(end);
+        nodeList.add(end);
         int k = end;
-        while(k != start){
+        while (k != start) {
             Vertex vK = graph.get(k);
-            int prevNode = 0;
-            for(int v : vK.getAdjacencyArray()){
-                if(pathCosts[v] + graph.get(k).getCost() == pathCosts[k]){
+            int prevNode = -1;
+            ArrayList<Integer> pathNodes = new ArrayList<>(1);
+            for (int v : vK.getAdjacencyArray()) {
+                if (!isIn(visitedNodes, v) && pathCosts[v] + graph.get(k).getCost() == pathCosts[k] ) {
                     pathCosts[v] = pathCosts[min] + graph.get(v).getCost();
                     prevNode = v;
+                    visitedNodes.add(prevNode);
+                    pathNodes.add(prevNode);
                 }
             }
-            nodeList.add(prevNode);
-            k = prevNode;
+            // abort if all neighbors were already visited
+            if(prevNode == -1)
+            {
+                System.out.println("Error -1");
+                return nodeList;
+            }
+
+            //choose node with lowest euclidian distance to start
+            double dMin = Double.POSITIVE_INFINITY;
+            int vMin = 0;
+            for(Integer v : pathNodes){
+                final double distance = Math.sqrt(Math.pow(v / heigth - start / heigth, 2) + Math.pow(v % heigth - start % heigth, 2));
+                if(distance < dMin){
+                    dMin = distance;
+                    vMin = v;
+                }
+            }
+
+            nodeList.add(vMin);
+            k = vMin;
         }
+        nodeList.add(start);
         return nodeList;
     }
 
-    private int selectDeleteMin(ArrayList<Integer> list){
+    private int selectDeleteMin(ArrayList<Integer> list) {
         int min = list.get(0);
         int index = 0;
-        for(int i = 0; i < list.size(); i++){
-            if (list.get(i) < min){
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) < min) {
                 min = list.get(i);
                 index = i;
             }
@@ -97,7 +119,17 @@ public class ScissorsFilter extends AbstractFilter {
     }
 
 
-    private int[] listToArray(ArrayList<Integer> list){
+    private boolean isIn(ArrayList<Integer> list, int searched) {
+        for (Integer integer : list) {
+            if (integer == searched) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private int[] listToArray(ArrayList<Integer> list) {
         int[] result = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             result[i] = list.get(i);
@@ -105,29 +137,68 @@ public class ScissorsFilter extends AbstractFilter {
         return result;
     }
 
+
     @Override
     public Image filter(Image input) {
-        Image output = new Grayfilter().filter(input);
+        Image grayImage = new Grayfilter().filter(input);
+        Image output = ImageFactory.getPrecision(input).rgb(input.getSize());
+        output = new CopyImageFilter_KB().filter(input);
         int width = input.getWidth();
         int height = input.getHeight();
+
+        // Get input for start an end point
+        DrawableImage drawImage = ImageFactory.bytePrecision()
+                .drawable(grayImage.asBufferedImage());
+
+        Graphics graphics = drawImage.getGraphics();
+        graphics.setColor(Color.GREEN);
+
+        // While the user selects points, continue drawing the line
+        List<Point> selections;
+        List<Point> points = new ArrayList<>();
+        Point previous = null;
+        do {
+            // Ask user for selection
+            selections = Controller.getCommunicationManager().getSelections("Please select point or close window.", 1, drawImage);
+            if(selections.size() > 0){
+                points.add(selections.get(0));
+            }
+
+            for(Point current : selections) {
+                if(previous == null)
+                    graphics.fillOval(current.x - 3, current.y - 3, 7, 7);
+                else
+                    graphics.drawLine(previous.x, previous.y, current.x, current.y);
+
+                previous = current;
+            }
+
+        } while(selections.size() > 0);
+
+
         double[][] horizontal = new SobelFilterHorizontal().applyConvolution(input);
         double[][] vertical = new SobelFilterVertical().applyConvolution(input);
         double[][] absValues = new SobelOperatorFilter().calculateAbsoluteGradient(horizontal, vertical, width, height);
 
         // start and end arrays first value is column, second is row
-        int[] start = new int[]{0, 0};
-        int[] end = new int[]{10, 10};
+        int[] start = new int[]{(int)points.get(0).getX(), (int)points.get(0).getY()};
+        int[] end = new int[]{(int)points.get(1).getX(), (int)points.get(1).getY()};
         ArrayList<Vertex> graph = createGraph(absValues, start, end, width, height);
-        ArrayList<Integer> path = executeDijkstra(graph, start[0]*width + start[1], end[0]*width + end[1]);
+        ArrayList<Integer> path = executeDijkstra(graph, start[0] * height + start[1], end[0] * height + end[1], height);
 
         //convert adjacency Matrix indices to image indices
         int[][] imagePath = new int[path.size()][2];
-        for(int i = 0; i < path.size(); i++){
-            imagePath[i][0] = path.get(i)/width;
-            imagePath[i][1] = path.get(i)%width;
+        for (int i = 0; i < path.size(); i++) {
+            imagePath[i][0] = path.get(i) / height;
+            imagePath[i][1] = path.get(i) % height;
             //set pixel of path to black
-            output.setValue(imagePath[i][0], imagePath[i][1], 0);
+            output.setValue(imagePath[i][0], imagePath[i][1], 0,255,0);
         }
+        System.out.println(path.size());
+        output.setValue(imagePath[0][0], imagePath[0][1], 0,0,255);
+        output.setValue(imagePath[imagePath.length-1][0], imagePath[imagePath.length-1][1], 0,0,255);
+        output.setValue(start[0], start[1], 255,0,0);
+        output.setValue(end[0], end[1], 255,0,0);
         return output;
     }
 }
